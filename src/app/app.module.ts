01706ecdeb957b13cf620298b1c159ed1296abf9
Ido Klimovsky <ido.klimovsky@adesso.de>
2018-11-04 20:11:03 +0100

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IngredientsComponentComponent } from './components/ingredients-component/ingredients-component.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { RecipesComponentComponent } from './components/recipes-component/recipes-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngredientsComponentComponent,
+    HeaderComponentComponent,
+    RecipesComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/ingredients-component/ingredients-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredients-component',
+  templateUrl: './ingredients-component.component.html',
+  styleUrls: ['./ingredients-component.component.css']
+})
+export class IngredientsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
