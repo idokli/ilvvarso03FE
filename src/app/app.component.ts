a0b11ff45e454df59c87ef786031e54682f35c3c
Lucas Larisch <s0558070@htw-berlin.de>
2018-11-05 19:19:14 +0100

    IngreatService mit aufrufbarer Methode, die Mock-Rezepte zurückgibt. Zunächst in Konstruktor von AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {IngreatService} from "./services/ingreat.service";
+import {Ingredient} from "./dataclasses/Ingredient";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ilvvarso03FE';
+
+  constructor(
+    private ingreatService: IngreatService
+  ) {
+    let i1 = new Ingredient();
+    i1.name = "Hallo";
+    let i2 = new Ingredient();
+    i2.name = "Angular";
+    console.log(ingreatService.reqRecipesByIngredients([i1, i2]));
+  }
+
 }
