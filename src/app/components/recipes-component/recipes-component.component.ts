8b670424e490156ffe7625de8105b71da2b12fe7
Ido Klimovsky <ido.klimovsky@adesso.de>
2018-12-09 21:56:55 +0100

    changed Input and created a mock of searchedIngredients

--- src/app/components/recipes-component/recipes-component.component.ts
+++ src/app/components/recipes-component/recipes-component.component.ts
@@ -10,16 +10,22 @@ import {HttpErrorResponse} from '@angular/common/http';
 })
 export class RecipesComponentComponent implements OnInit {
 
-  @Input() recipes: Recipe[];
+  @Input() searchedIngredients: string[];
+
+  recipes: Recipe[];
 
   constructor( private ingreatService: IngreatService) { }
 
   ngOnInit() {
     this.ingreatService.reqRecipesByIngredients(null).subscribe( data => {
+    // this.ingreatService.reqRecipesByIngredients(searchedIngredients).subscribe( data => {
       this.recipes = data;
     }, (error: HttpErrorResponse) => {
       console.log(`Backend returned code ${error.status}, body was: ${error.error}`);
     });
+
+    //TODO remove this line after the api works
+    this.searchedIngredients = ['Mett', 'Petersilie','Brezeln'];
   }
 
 }
