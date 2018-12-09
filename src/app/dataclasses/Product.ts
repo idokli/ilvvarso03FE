4a0e970b94ab2283122b4124df776daab8b3ea0b
Ido Klimovsky <ido.klimovsky@adesso.de>
2018-12-09 21:57:28 +0100

    added constructor for mock object

--- src/app/dataclasses/Product.ts
+++ src/app/dataclasses/Product.ts
@@ -1,6 +1,14 @@
 import {Supermarket} from './Supermarket';
 
 export class Product {
+
+
+  constructor(name, supermarket, price) {
+    this.name = name;
+    this.supermarket = supermarket;
+    this.price = price;
+  }
+
   name: string;
   supermarket: Supermarket;
   price: number;
